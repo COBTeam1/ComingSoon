24efa004b038f1d8c346f4aa37b8eeaf95ae64d0
Ricky Nguyen <105029551+nguyricky@users.noreply.github.com>
2024-02-23 21:19:34 -0800

    fix: arrow absolution position issues

--- src/components/pages/construction.jsx
+++ src/components/pages/construction.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import osu_logo from '/osu_logo.png';
-import arrow from '../assets/arrow.png';
+import arrow from '../assets/arrow-v2.svg';
 
 const words = ['Beavers.', 'Students.', 'Professors.', 'Faculty.', 'Alumni.'];
 
@@ -55,36 +55,39 @@ const Construction = () => {
     return (
         <div className="flex flex-col h-screen bg-neutral">
             {/* <nav className='container mx-auto flex justify-between items-center my-8'>
+                <img src={osu_logo} alt="osu logo" className="w-48" />
                 <ul className="flex justify-center space-x-4">
 
                 </ul>
             </nav> */}
             <div className="flex-grow flex flex-col items-center">
-                <h2 className="tracking-widest text-center text-accent text-2xl md:text-[48px] font-black mb-6 md:mb-10 mt-[200px]; md:mt-[265px]">Website under Construction</h2>
-                <div className="text-center w-3/5 md:w-[35%]">
+                <h2 className="tracking-widest text-center text-accent text-xl md:text-[48px] font-black mb-6 md:mb-10 mt-[200px] md:mt-[265px]">Website under Construction</h2>
+                <div className="text-center w-4/5 md:w-[35%]">
                     <p className="construction-text">{wip}</p> 
                 </div>
-                <form onSubmit={handleSubmit} className="mt-[60px] flex">
-                    <input 
-                        type="email" 
-                        placeholder="Enter your email" 
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="text-md text-accent w-[400px] p-2 rounded-l-md border-r-0 border-primary focus:outline-none focus:ring-1 focus:ring-primary transition duration-300 ease-in-out shadow"
-                        required
-                    />
-                    <button 
-                        type="submit" 
-                        className="bg-primary text-white tracking-wide font-medium w-32 p-2 rounded-r-md border-l-0 hover:bg-accent transition duration-300 ease-in-out shadow"
-                    >
-                        Notify Me
-                    </button>
-                </form>
-                <img src={arrow} 
-                    alt="arrow" 
-                    className="absolute w-8 md:w-12" 
-                    style={{ transform: 'rotate(90deg)', top: '460px', right: '1185px' }}
-                />
+                <div className='flex flex-row'>
+                    <img src={arrow} 
+                        alt="arrow" 
+                        className="w-8 md:w-12 mt-6 mr-4 hidden md:block" 
+                        style={{ transform: 'rotate(75deg)'}}
+                    />  
+                    <form onSubmit={handleSubmit} className="mt-[60px] flex">
+                        <input 
+                            type="email" 
+                            placeholder="Enter your email" 
+                            value={email} 
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="text-sm md:text-md text-accent sm:w-[200px] md:w-[400px] p-2 rounded-l-md border-r-0 border-primary focus:outline-none focus:ring-1 focus:ring-primary transition duration-300 ease-in-out shadow"
+                            required
+                        />
+                        <button 
+                            type="submit" 
+                            className="bg-primary text-white text-sm md:text-md tracking-wide font-small md:font-medium sm:w-20 md:w-32 p-2 rounded-r-md border-l-0 hover:bg-accent transition duration-300 ease-in-out shadow"
+                        >
+                            Notify Me
+                        </button>
+                    </form>
+                </div>
             </div>
             <div className="text-center pb-4 md:pb-8">
                 <p className="construction-text md:text-[15px]">
